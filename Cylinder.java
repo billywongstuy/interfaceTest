c563392ab7059c15d04bd94e8404d8d612311dd5
Billy Wong <bwong5@stuy.edu>
2015-12-10 12:13:58 -0500

    added interfaces, changed toString, and added getArea and getVolumemethods

--- Cylinder.java
+++ Cylinder.java
@@ -1,17 +1,21 @@
-public class Cylinder extends Circle{
+public class Cylinder extends Circle implements Volume{
 
-  private double height;
+    private double height;
 
-  public Cylinder(double radius, double height,String name){
-    super(radius,name);
-    this.height = height;
-  }
+    public Cylinder(double radius, double height,String name){
+	super(radius,name);
+	this.height = height;
+    }
 
-  public double getHeight(){
-    return height;
-  }
+    public double getHeight(){
+	return height;
+    }
 
-  public String toString(){
-    return "FIXME!";
-  }
+    public double getVolume() {
+	return super.getArea()*getHeight();
+    }
+
+    public String toString(){
+	return "Cylinder "+getName()+" with radius of "+getRadius()+" and height of " + getHeight();
+    }
 }
--- Sphere.java
+++ Sphere.java
@@ -1,10 +1,14 @@
-public class Sphere extends Circle{
+public class Sphere extends Circle implements Volume{
 
-  public Sphere(double radius, String name){
-    super(radius,name);
-  }
+    public Sphere(double radius, String name){
+	super(radius,name);
+    }
 
-  public String toString(){
-    return "FIXME!";
-  }
+    public double getVolume() {
+	return (4/3)*Math.PI*Math.pow(getRadius(),3);
+    }
+
+    public String toString(){
+	return "Sphere "+getName()+" with radius of "+getRadius();
+    }
 }
